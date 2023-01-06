5ce76ee908c504afc4e472e1b8723e0613690277
David Lei <DavidtheDefender2003@gmail.com>
2023-01-06 13:51:28 -0800

    Add event listeners for number buttons
    Event listeners will add a number to number display

--- script.js
+++ script.js
@@ -23,3 +23,43 @@ const answerDisplay = document.querySelector(".answer-display");
 
 // Solution for current expression
 let solution = null;
+
+oneBtn.addEventListener("click", () => {
+    answerDisplay.textContent += "1";
+});
+
+twoBtn.addEventListener("click", () => {
+    answerDisplay.textContent += "2";
+});
+
+threeBtn.addEventListener("click", () => {
+    answerDisplay.textContent += "3";
+});
+
+fourBtn.addEventListener("click", () => {
+    answerDisplay.textContent += "4";
+});
+
+fiveBtn.addEventListener("click", () => {
+    answerDisplay.textContent += "5";
+});
+
+sixBtn.addEventListener("click", () => {
+    answerDisplay.textContent += "6";
+});
+
+sevenBtn.addEventListener("click", () => {
+    answerDisplay.textContent += "7";
+});
+
+eightBtn.addEventListener("click", () => {
+    answerDisplay.textContent += "8";
+});
+
+nineBtn.addEventListener("click", () => {
+    answerDisplay.textContent += "9";
+});
+
+zeroBtn.addEventListener("click", () => {
+    answerDisplay.textContent += "0";
+});
